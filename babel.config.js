84ecb6943ee047fe04b218780e1cd7a6c5a81c47
Kirill Tikhonov <lliriq@me.com>
2021-12-29 22:22:25 +0300

    Babel config

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ module.exports = (api) => ({
       "@babel/preset-env",
       {
         useBuiltIns: "entry",
+        corejs: "3.20.1",
         targets: api.caller((caller) => caller && caller.target === "node")
           ? { node: "current" }
           : { chrome: "58", ie: "11" },
