f6491e57e97b9487c3e2d5f15f79cffb5125dfdc
Kirill Tikhonov <lliriq@me.com>
2022-01-04 23:29:34 +0500

    Carousel JS

--- src/carousel.js
+++ src/carousel.js
@@ -1,36 +1,64 @@
 import "./css/index.css";
 
-let slideIndex = 1;
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+import Media1 from "./assets/1media.jpg";
+import Media2 from "./assets/2-media.png";
+import Media3 from "./assets/3-media.png";
 
-prevButton.addEventListener("click", minusSlides);
-nextButton.addEventListener("click", plusSlides);
+function carouselInit(el) {
+  el.innerHTML = `
+  <div class="carousel__slide"></div>
+  <div class="carousel__slide"></div>
+  <div class="carousel__slide"></div>
+  <a class="prev">&#10094;</a>
+  <a class="next">&#10095;</a>
+  `;
 
-showSlides(slideIndex);
+  const slide1 = new Image();
+  slide1.src = Media1;
+  document.querySelectorAll(".carousel__slide")[0].append(slide1);
 
-function plusSlides() {
-  slideIndex += 1;
+  const slide2 = new Image();
+  slide2.src = Media2;
+  document.querySelectorAll(".carousel__slide")[1].append(slide2);
+
+  const slide3 = new Image();
+  slide3.src = Media3;
+  document.querySelectorAll(".carousel__slide")[2].append(slide3);
+
+  let slideIndex = 1;
+  const prevButton = el.querySelector(".prev");
+  const nextButton = el.querySelector(".next");
+
+  prevButton.addEventListener("click", minusSlides);
+  nextButton.addEventListener("click", plusSlides);
+
   showSlides(slideIndex);
+
+  function plusSlides() {
+    slideIndex += 1;
+    showSlides(slideIndex);
+  }
+
+  function minusSlides() {
+    slideIndex -= 1;
+    showSlides(slideIndex);
+  }
+
+  function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("carousel__slide");
+
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i += 1) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
 }
 
-function minusSlides() {
-  slideIndex -= 1;
-  showSlides(slideIndex);
-}
-
-function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("carousel__slide");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i += 1) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
+carouselInit(document.querySelector(".carousel"));
